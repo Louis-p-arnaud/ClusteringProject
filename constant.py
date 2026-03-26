b7cebb0d8eb4c402cf89e312a55975d374be4d85
DorafBG <58978093+DorafBG@users.noreply.github.com>
2026-03-26 18:22:59 +0100

    generalisation pipeline et dashboarding

--- constant.py
+++ constant.py
@@ -8,5 +8,5 @@ PROJECT_DIR = os.path.abspath(SCRIPT_DIR)  # Assure que nous sommes dans le rép
 
 PATH_ALGO = os.path.join(PROJECT_DIR, "Algos")
 PATH_OUTPUT = os.path.join(PATH_ALGO, "kmeans_algo", "output")
-PATH_DATASET = os.path.join(PROJECT_DIR, "dataset")
+PATH_DATASET = os.environ.get("DATA_PATH", os.path.join(PROJECT_DIR, "data", "test"))
 MODEL_CLUSTERING = "kmeans"
